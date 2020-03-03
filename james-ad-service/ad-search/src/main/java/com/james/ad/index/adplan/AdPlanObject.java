b24c0e769ad6236793005edb5843b9bb25b13147
J.Z <yiz132@pitt.edu>
2020-03-02 23:48:07 -0500

    indexing ad plan

---
+++ james-ad-service/ad-search/src/main/java/com/james/ad/index/adplan/AdPlanObject.java
@@ -0,0 +1,37 @@
+package com.james.ad.index.adplan;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.Date;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class AdPlanObject {
+
+    private Long planId;
+    private Long userId;
+    private Integer planStatus;
+    private Date startDate;
+    private Date endDate;
+
+    public void update(AdPlanObject newObject){
+        if (newObject.getPlanId() != null) {
+            this.planId = newObject.getPlanId();
+        }
+        if (newObject.getUserId() != null) {
+            this.userId = newObject.getUserId();
+        }
+        if (newObject.getPlanStatus() != null) {
+            this.planStatus = newObject.getPlanStatus();
+        }
+        if (newObject.getStartDate() != null) {
+            this.startDate = newObject.getStartDate();
+        }
+        if (newObject.getEndDate() != null) {
+            this.endDate = newObject.getEndDate();
+        }
+    }
+}
